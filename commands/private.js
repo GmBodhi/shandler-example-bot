912e0810a4b26e3ebe63b18d358fbf707ab2b8eb
Crispy <61794525+Crispy-Cream@users.noreply.github.com>
2021-06-02 23:41:58 -0500

    Delete commands directory

--- commands/private.js
+++
@@ -1,9 +0,0 @@
-//private.js
-module.exports = {
-  name: 'private',// Will default to filename if this is empty.
-  description: 'Private Response.',//Default: "An awesome command..!"
-  guilds: ['789259215868395552'],  /*This is for guild specific command registration. If this is empty, the command will be registered globally*/
-  async run({ interaction, client }) {
-    interaction.reply("Private Message", { flags: 64 }) //Send the interaction message, but as a private message.
-  }
-}
