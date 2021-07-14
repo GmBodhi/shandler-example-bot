ecc82fbadc2413b0ca6265981956887c53514b0e
Crispy <61794525+Crispy-Cream@users.noreply.github.com>
2021-07-14 16:38:31 -0500

    Create actionrow.js

---
+++ commands/actionrow.js
@@ -0,0 +1,22 @@
+const discord = require('discord.js')
+module.exports = {
+    guilds: ['789259215868395552'],
+    name: 'actionrow',
+    description: 'Reply to the interaction with an button(s) using action rows.',
+    async run({ interaction, client }) {
+        let cmp = [
+            {
+                "type": 1, "components": [
+                    { "type": 2, "style": 1, "label": "Button 1", "custom_id": "button_1" },
+                ]
+            },
+            {
+                "type": 1, "components": [
+                    { "type": 2, "style": 4, "label": "Button 2", "custom_id": "button_2" }
+                ]
+            }
+        ]; // Creating the buttons and two action rows.
+
+        interaction.reply("Buttons!", { components: cmp, type: 4, }); // Send the reply.
+    }
+}
