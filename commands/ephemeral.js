30465b1c1a1e1dbbd13765bd67a804f9053f4636
Crispy <61794525+Crispy-Cream@users.noreply.github.com>
2021-07-14 16:25:53 -0500

    Delete commands directory

--- commands/ephemeral.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ephemeral',
-    description: 'Sphemeral Response.',
-    guilds: ['789259215868395552'],
-    async run({ interaction, client }) {
-        interaction.reply("Ephemeral Message", { flags: 64 }) //Send the interaction message, but as a private message.
-    }
-}
--- commands/reply.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'reply',
-    description: 'Reply to the interaction',
-    guilds: ['789259215868395552'],
-    async run({ interaction, client }) {
-        interaction.reply('Bello!').then(console.log);
-    }
-}
